e75538965a7622d09d8e93ee06112fd3ad2b19d0
PHANNA11 <phaphanna2018@gmail.com>
2022-08-15 19:50:20 +0700

    config android

---
+++ android/app/src/main/kotlin/com/example/camera7_8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camera7_8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Camera'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+File? file;
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          child: file == null
+              ? SizedBox()
+              : Image(image: FileImage(File(file!.path))),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                getgallary();
+              });
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.image),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                getCamera();
+              });
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.camera_alt),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> getCamera() async {
+    final files = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      file = File(files!.path);
+    });
+  }
+
+  Future<void> getgallary() async {
+    final files = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = File(files!.path);
+    });
+  }
+}
